08882e6c201101d169d9e95731a4ecadbde29225
Kevin Smith <kevinstewartsmith@gmail.com>
2023-05-17 21:30:12 +0700

    Customize hear.

--- app/header.js
+++ app/header.js
@@ -1,13 +1,35 @@
+"use client"
 import React from "react";
 import FlashOnIcon from '@mui/icons-material/FlashOn';
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom"
+import { Lobster } from '@next/font/google';
+const lobster = Lobster({ 
+  subsets: ['latin'], 
+  weight: '400',
+  // display: 'swap',
+  // family: 'Landrina Outline, cursive',
+  // style: 'normal',
+  fontStretch: 'expanded',
+  fontStyle: 'italic',
+  fontWeight: 'bold',
+  
+ })
+//  color: #fff;
+//  font-family: "Lobster", cursive;
+//  font-size: 2.8rem;
+//  font-weight: bold;
+//  font-style: italic;
+//  font-stretch: expanded;
+//  font-family: 'Londrina Outline', cursive;
+//import globals css
+import './globals.css'
+
 
 function Header() {
-  const navigate = useNavigate()
+ 
+  
   return (
     <header>
-      <div  onClick={() => { navigate("/") }}><h1>Flashy<div className="lightning"><FlashOnIcon fontSize="56rem"/></div>McFlasherson</h1></div>
+      <div  onClick={() => {}}><h1 className={lobster.className}>Flashy<div className="lightning"><FlashOnIcon fontSize="56rem"/></div>McFlasherson</h1></div>
     </header>
   );
 }
