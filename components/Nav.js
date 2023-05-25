4afbbc668723e318ebd69d6fc97e6dc98a545646
Kevin Smith <kevinstewartsmith@gmail.com>
2023-05-26 01:01:54 +0700

    Adjust navbar and add Next Auth capabilitites.

--- components/Nav.js
+++ components/Nav.js
@@ -31,7 +31,7 @@ function Header() {
  
   
   return (
-    <nav className="flex-between w-full mb-16 pt-3">
+    <nav className="flex-between w-full mb-16 p-4">
       {/* <div  className="italics" onClick={() => {}}><h1 className={lobsterTwo.className}>Flashy<div className="lightning"><FlashOnIcon fontSize="56rem"/></div>McFlasherson</h1></div> */}
         <Link href="/" className="flex gap-2 flex-center">
             <Image src={'/flashy-logo.png'} width={423.387} height={70} alt="Flashy McFlasherson" />
@@ -62,7 +62,7 @@ function Header() {
                         </button>  
                         <Link href="/profile">
                             <Image
-                                src="/assets/images/logo.svg"
+                                src="/assets/images/logo.png"
                                 alt="Profile"
                                 width={37}
                                 height={37}
@@ -79,7 +79,7 @@ function Header() {
                                     type="button"
                                     key={provider.name}
                                     onClick={() => signIn(provider.id)}
-                                    className="black_btn"
+                                    className="white_btn"
                                 >
                                    Sign In
                                 </button>
@@ -87,6 +87,67 @@ function Header() {
                             ))}
                     </>
                 )}
+            </div>
+
+
+             {/* mobile navigation */}
+             <div className="sm:hidden flex relative">
+                {session?.user ? (
+                    <div className="flex">
+                    <Image
+                        src={session?.user.image}
+                        alt="Profile"
+                        width={37}
+                        height={37}
+                        className="rounded-full"
+                        onClick={() => setToggleDropDown((prev) => !prev)}
+                    />
+                    
+                    {toggleDropDown && (
+                        <div className="dropdown">
+                            <Link 
+                                className="dropdown_link"
+                                href="/profile"
+                                onClick={() => setToggleDropDown(false)}
+                            >
+                                My Profile
+                            </Link>
+                            <Link 
+                                className="dropdown_link"
+                                href="/create-prompt"
+                                onClick={() => setToggleDropDown(false)}
+                            >
+                                Create Prompt
+                            </Link>
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    setToggleDropDown(false)
+                                    signOut()
+                                }}
+                                className="mt-5 w-full black_btn"
+                            >
+                                Sign Out
+                            </button>
+                        </div>
+                    )}
+
+                    </div>
+                ): (
+                    <>
+                    {providers && 
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="black_btn"
+                                >
+                                   Sign In
+                                </button>
+
+                    ))} 
+                </>  )}
             </div>
     </nav>
   );
