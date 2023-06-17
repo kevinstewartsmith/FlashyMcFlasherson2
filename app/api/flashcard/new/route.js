8b8351da4d64c93da64b4707dbf54b7b992a89d0
Kevin Smith <kevinstewartsmith@gmail.com>
2023-06-17 19:51:37 -0400

    Flash cards load when a collection is clicked.

--- app/api/flashcard/new/route.js
+++ app/api/flashcard/new/route.js
@@ -4,18 +4,18 @@ import FlashCard from "@models/FlashCard";
 
 export const POST = async (request) => {
     //const { userId, prompt, tag } = await request.json();
-    const { userId, front, back, collection } = await request.json();
+    const { userId, front, back, collectionID } = await request.json();
     console.log(userId);
     console.log(front);
     console.log(back);
-    console.log(collection);
+    console.log(collectionID);
     
     
     try {
         await connectToDB();
 
         // Find the existing prompt by ID
-        const existingCollection = await FlashCardsCollection.findById(collection).populate('creator');
+        const existingCollection = await FlashCardsCollection.findById(collectionID).populate('creator');
 
         if (!existingCollection) {
             return new Response("Collection not found", { status: 404 });
@@ -27,7 +27,7 @@ export const POST = async (request) => {
             creator: userId,
             front: front,
             back: back,
-            collectionID: existingCollection._id
+            collectionID
         })
 
         await newFlashCard.save();
