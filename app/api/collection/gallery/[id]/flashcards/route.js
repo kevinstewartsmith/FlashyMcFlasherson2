b9ed265f3eb9d8aa313eeee01be84c9ffe318c53
Kevin Smith <kevinstewartsmith@gmail.com>
2023-06-21 16:46:16 -0400

    Delete collection functionality added.

--- app/api/collection/gallery/[id]/flashcards/route.js
+++ app/api/collection/gallery/[id]/flashcards/route.js
@@ -20,3 +20,22 @@ export const GET = async (request, { params }) => {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
 }
+
+// export const DELETE = async (request, { params }) => {
+//     try {
+
+//         await connectToDB();
+//         console.log("fetching: " + params.id);
+//         const flashCard = await FlashCard.find({flashCardID: params.id})
+//         if (!flashCard) return new Response("Flashcards Not Found", { status: 404 });
+
+        
+//         console.log(flashCard);
+//         console.log(flashCard.length);
+
+//         return new Response(JSON.stringify(flashCard), { status: 200 })
+//     } catch (error) {
+//         console.log("DELETE api/collection/[id]/flashcards.js error: " + error);
+//         return new Response("Failed to fetch all prompts", { status: 500 })
+//     }
+//}
--- app/api/collection/gallery/[id]/route.js
+++ app/api/collection/gallery/[id]/route.js
@@ -15,3 +15,23 @@ export const GET = async (request, { params }) => {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
 } 
+
+// export const DELETE = async (request, { params }) => {
+//     try {
+
+//         await connectToDB();
+//         console.log("fetching: " + params.id);
+//         const collection = await FlashCardsCollection.findById(params.id).populate('creator')
+//         if (!collection) return new Response("Collection Not Found", { status: 404 });
+
+        
+//         console.log(collection);
+//         console.log(collection.length);
+
+//         return new Response(JSON.stringify(collection), { status: 200 })
+//     } catch (error) {
+//         console.log("DELETE api/collection/[id]/flashcards.js error: " + error);
+//         return new Response("Failed to fetch all prompts", { status: 500 })
+//     }
+// }
+
