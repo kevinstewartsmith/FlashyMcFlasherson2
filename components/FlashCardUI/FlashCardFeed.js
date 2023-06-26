03d5628a2eb7f0413c40cb46ac891c3f94efb2eb
Kevin Smith <kevinstewartsmith@gmail.com>
2023-06-26 13:05:37 -0400

    Make dynamic text in flashcards in flashcard feed.

--- components/FlashCardUI/FlashCardFeed.js
+++ components/FlashCardUI/FlashCardFeed.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Grid } from '@mui/material'
 import Note from '../CollectionUI/note'
 import FlashCard from './FlashCard'
+import { Height } from '@mui/icons-material'
 
 const FlashCardList = ({data}) => {
     
@@ -11,19 +12,27 @@ const FlashCardList = ({data}) => {
             <Grid
                 container
                 //rowSpacing={1}
-                spacing={5}
+                spacing={0}
                 //columnSpacing={{ xs: 1, sm: 2, md: 3 }}
                 justify="space-evenly"
                 alignItems="center"
             >
                 { data.map((flashCard,idx) => (
-                    <Grid item padding={1} xs={12} sm={6} md={4} key={idx}>
-                        <FlashCard 
+                    <Grid item padding={2} xs={12} sm={6} md={4} key={idx}>
+                        {/* <FlashCard 
                             key={flashCard._id}
                             id={flashCard._id}
                             front={flashCard.front}
                             back={flashCard.back}
-                        />  
+                        />  */}
+                        {/* <div style={{backgroundColor: "blue", height: "40vh", width:"100%" }}> */}
+                            <FlashCard 
+                                key={flashCard._id}
+                                id={flashCard._id}
+                                front={flashCard.front}
+                                back={flashCard.back}
+                            />
+                        {/* </div>  */}
                     </Grid>
                 ))} 
             </Grid>
